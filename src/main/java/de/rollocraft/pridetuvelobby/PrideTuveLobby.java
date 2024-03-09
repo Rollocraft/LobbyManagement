9dd329cf2f1c24bfec20b7f7d4be324e1dea0892
rollocraft <Rollocraft117@gmail.com>
2024-03-09 16:58:54 +0100

    Version 0

---
+++ src/main/java/de/rollocraft/pridetuvelobby/PrideTuveLobby.java
@@ -0,0 +1,24 @@
+package de.rollocraft.pridetuvelobby;
+
+/*
+    *  PrideTuveLobby
+    * Version: 0.0.1
+    * Author: Rollocraft
+    * 09.03.2024
+ */
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrideTuveLobby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
